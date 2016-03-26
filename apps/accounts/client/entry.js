4a267b847a8044cce0ec7a6b972dadc0fa156662
Mark Swedberg <swedy13@gmail.com>
2016-03-25 23:52:32 -0500

    Mobile first setup

---
+++ apps/accounts/client/entry.js
@@ -0,0 +1,27 @@
+Template.AccountsEntry.events({
+  'click .signin': function() {
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+
+    Meteor.loginWithPassword(email, password, function(error) {
+      if (error) {
+        console.log(error.reason);
+      }
+    });
+  },
+  
+  'click .signup': function() {
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+
+    Accounts.createUser({
+      email: email,
+      password: password
+    }, function(error) {
+      if (error) {
+        console.log(error.reason);
+      }
+    });
+    console.log('complete');
+  }
+});

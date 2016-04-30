6276cc1accf834317b98a40a3f056c5a311eb308
Mark Swedberg <Mark Swedberg>
2016-04-30 17:49:21 -0500

    I hate git more than anything

--- apps/subscriptions/client/components/subscriptions.js
+++ apps/subscriptions/client/components/subscriptions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*Template.NavMenu.helpers({
 	*  subsPath: function() {
 	*    var routeName = "subscriptions";
@@ -7,13 +6,3 @@
 	*    return path;
 	*  }
 	*});*/
-=======
-Template.NavMenu.helpers({
-  subsPath: function() {
-    var routeName = "subscriptions";
-    var path = FlowRouter.path(routeName);
-
-    return path;
-  }
-});
->>>>>>> 99bee4fff07a6a0b89072c51e8eec92b93f5358e
